b4f80a46eb6aff597865c17ac64b6e983fc43aaa
SvetlanaMd <SvetlanaMoldavskaya@users.noreply.github.com>
2020-06-24 10:56:21 +0300

    Add files via upload

---
+++ String_to_Int_3.py
@@ -0,0 +1,8 @@
+# 3. User inputs n number. Find out the sum of n + nn + nnn. E.g.: user inputs 3. Result: 3 + 33 + 333 = 369.
+
+n = str(input("What is your number: "))
+print (int(n) + int(n*2) + int(n*3))
+
+
+
+
